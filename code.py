17506c36ee9715b4b75498b16a8f0e4cf36104b9
Siva Sandeep Reddy <48346459+rssreddy@users.noreply.github.com>
2020-11-21 16:31:01 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+def subString(Str,n):
+    l=[]
+    for Len in range(1,n + 1):
+        for i in range(n - Len + 1):
+            j = i + Len - 1
+            l.append(Str[i:j+1])
+    return l
+s=input()
+l=subString(s,len(s))
+k=max([len(set(list(i))) for i in l])
+print(k)
