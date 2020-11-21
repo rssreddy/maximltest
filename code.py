2e12cd8c4f63f08aa898476ea4cb52ade3f90fd4
Siva Sandeep Reddy <48346459+rssreddy@users.noreply.github.com>
2020-11-21 20:18:13 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,30 @@
-def subString(Str,n):
-    l=[]
-    for Len in range(1,n + 1):
-        for i in range(n - Len + 1):
-            j = i + Len - 1
-            l.append(Str[i:j+1])
-    return l
-s=input()
-l=subString(s,len(s))
-k=max([len(set(list(i))) for i in l])
-print(k)
+from collections import defaultdict
+import sys
+
+
+def SmallestSubString(S):
+    distinct_count = len(set(list(S)))
+    n = len(S)
+    freq = defaultdict(int)
+    start_idx = 0
+    min_len = sys.maxsize
+    distinct_till_here = 0
+    for j in range(n):
+        freq[S[j]] += 1
+        if freq[S[j]] == 1:
+            distinct_till_here += 1
+
+        if distinct_count == distinct_till_here:
+            while freq[S[start_idx]] > 1:
+                if freq[S[start_idx]] > 1:
+                    freq[S[start_idx]] -= 1
+                start_idx += 1
+
+            curr_len = j - start_idx + 1
+            min_len = min(min_len, curr_len)
+    return min_len
+
+
+S = input()
+ans = SmallestSubString(S)
+print(ans)
